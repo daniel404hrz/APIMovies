604894b77c52f986ba66ce5882e7ce2cac559e5f
danielhp404 <daniel.404hrz@gmail.com>
2024-02-29 20:19:51 -0600

    renta put and gets complete

--- src/controllers/renta.controller.js
+++ src/controllers/renta.controller.js
@@ -2,17 +2,15 @@ import { db } from "../db.js";
 import { ref, push, set, get } from "firebase/database";
 
 export const createRentals = async (req, res) => {
-  const { movieID, userID} = req.body;
+  const { movieID, userID } = req.body;
 
   try {
     const userRef = ref(db, `usuarios/${userID}`);
     const movieRef = ref(db, `peliculas/${movieID}`);
 
-  
     const rentalRef = push(ref(db, "alquileres"));
     const rentalId = rentalRef.key;
 
- 
     const movieSnapshot = await get(movieRef);
     const movieDetails = {
       movieID: movieSnapshot.key,
@@ -24,10 +22,9 @@ export const createRentals = async (req, res) => {
     const returnDate = new Date(today);
     returnDate.setDate(today.getDate() + 3);
 
-    
     await set(rentalRef, {
-      user: userRef.key, 
-      movie: movieRef.key, 
+      user: userRef.key,
+      movie: movieRef.key,
       rentalDate: today.toISOString().split("T")[0],
       returnDate: returnDate.toISOString().split("T")[0],
       penalty: 0, // Inicialmente sin penalización
@@ -45,4 +42,23 @@ export const createRentals = async (req, res) => {
     console.error("Error al registrar el alquiler:", error.message);
     res.status(500).json({ mensaje: "Error al registrar el alquiler" });
   }
+};
+export const getRentas = async (req,res) => {
+    const moviesRef = ref(db, "alquileres");
+
+
+    await get(moviesRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          res.status(201).json(data);
+        } else {
+          console.log('No hay datos en la referencia "alquileres".');
+          res.status(201).json({});
+        }
+      })
+      .catch((error) => {
+        console.error("Error al obtener datos:", error.message);
+        res.status(404).json({ mensaje: error.message });
+      });
 };
